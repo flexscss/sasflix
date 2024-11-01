81ee8875eb7337dfc965ed1dc753ab267bcbd272
toda1337 <toda.corsa@gmail.com>
2024-11-02 02:05:23 +0300

    Added image field for useSeo composable and change meta title

--- composables/useSeo.ts
+++ composables/useSeo.ts
@@ -3,13 +3,13 @@ import { useRuntimeConfig, useSeoMeta } from '#imports'
 interface Meta {
   title: string
   description: string
-  image: string
+  image?: string
 }
 
 export function useSeo(meta?: Meta) {
   const { public: { BASE_URL, META_TITLE, META_DESCRIPTION, META_IMAGE } } = useRuntimeConfig()
   return useSeoMeta({
-    title: meta?.title || META_TITLE,
+    title: meta?.title ? `${META_TITLE} - ${meta.title}` : META_TITLE,
     ogTitle: meta?.title || META_TITLE,
     description: meta?.description || META_DESCRIPTION,
     ogDescription: meta?.description || META_DESCRIPTION,
