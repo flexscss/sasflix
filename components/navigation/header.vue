d442bb80542b456ff2dfb460cb5ccfbd1b0768fb
toda1337 <toda.corsa@gmail.com>
2024-11-02 02:34:19 +0300

    Added header title

--- components/navigation/header.vue
+++ components/navigation/header.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    Header component
+    <div
+      class="container font-display font-bold text-header
+      text-center text-accent-primary pt-2.5 pb-12"
+    >
+      ikakprosto.ru
+    </div>
   </div>
 </template>
