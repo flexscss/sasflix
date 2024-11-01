655843514304496e457ac99af597e0e5fe1d41a5
toda1337 <toda.corsa@gmail.com>
2024-11-02 02:03:36 +0300

    Added icon component and like and dislike icons with 2 types

--- components/shared/ui/icon.vue
+++ components/shared/ui/icon.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-    Icon component
-    <component :is="props.name" />
-  </div>
+  <component :is="iconComponent" :class="props.class" />
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  name: {
-    type: String,
-    required: true
-  }
+import dislike from './icons/dislike.vue'
+import dislikeFilled from './icons/dislikeFilled.vue'
+import like from './icons/like.vue'
+import likeFilled from './icons/likeFilled.vue'
+
+const props = withDefaults(defineProps<{
+  name: IconName
+  class?: string
+}>(), {
+  class: 'w-3.5'
 })
+
+const icons = {
+  like,
+  likeFilled,
+  dislike,
+  dislikeFilled
+} as const
+
+type IconName = keyof typeof icons
+const iconComponent = computed(() => icons[props.name])
 </script>
