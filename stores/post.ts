3c4cb104bcff0e0c14690a12d412301be3241d64
toda1337 <toda.corsa@gmail.com>
2024-11-02 02:09:16 +0300

    Added store for Post, get posts and single post functions, get comments function, set reactions to cookie, set deleted comments to cookie

--- stores/post.ts
+++ stores/post.ts
@@ -1,3 +1,5 @@
+import { useCookie } from '#app'
+
 export const usePostStore = defineStore('post', () => {
   const comments = ref([] as PostComment[])
   interface ListResponseWithPostComments extends ListResponse {
@@ -8,6 +10,59 @@ export const usePostStore = defineStore('post', () => {
     if (data.value)
       comments.value = data.value?.comments
     return comments
+  }
+
+  interface RemovedComment {
+    [id: number]: number[]
+  }
+  const getRemovedCommentsFromStorage = (): RemovedComment => {
+    const removedComments = useCookie<RemovedComment>('APP_removedComments')
+    if (removedComments.value)
+      return removedComments.value
+    return {}
+  }
+
+  const setRemovedCommentsToStorage = (removedComments: RemovedComment) => {
+    const cookie = useCookie<RemovedComment>('APP_removedComments')
+    cookie.value = removedComments
+  }
+
+  const setRemovedComments = (postId: number) => {
+    const removedComments = getRemovedCommentsFromStorage()
+    const removedCommentsOfCurrentPost = removedComments[postId] || []
+    removedCommentsOfCurrentPost.forEach((commentId) => {
+      const comment = comments.value.find(item => item.id === commentId)
+      if (comment) {
+        comment.isRemoved = true
+      }
+    })
+  }
+
+  const removeComment = (commentId: number, postId: number) => {
+    const removedComments = getRemovedCommentsFromStorage()
+    const removedCommentsOfCurrentPost = removedComments[postId] || []
+    const commentIndex = removedCommentsOfCurrentPost.indexOf(commentId)
+    if (commentIndex === -1) {
+      removedCommentsOfCurrentPost.push(commentId)
+      removedComments[postId] = removedCommentsOfCurrentPost
+      setRemovedCommentsToStorage(removedComments)
+      const comment = comments.value.find(item => item.id === commentId)
+      if (comment)
+        comment.isRemoved = true
+    }
+  }
+
+  const restoreComment = (commentId: number, postId: number) => {
+    const removedComments = getRemovedCommentsFromStorage()
+    const removedCommentsOfCurrentPost = removedComments[postId] || []
+    const commentIndex = removedCommentsOfCurrentPost.indexOf(commentId)
+    if (commentIndex !== -1) {
+      removedCommentsOfCurrentPost.splice(commentIndex, 1)
+      setRemovedCommentsToStorage(removedComments)
+      const comment = comments.value.find(item => item.id === commentId)
+      if (comment)
+        comment.isRemoved = false
+    }
   }
 
   const post = ref({} as Post)
@@ -28,9 +83,91 @@ export const usePostStore = defineStore('post', () => {
   const getPosts = async () => {
     const { data } = await useApi<ListResponseWithPosts>('/posts?limit=5')
     if (data.value)
-      posts.value = data.value?.posts
+      posts.value = data.value.posts
     return posts
   }
 
-  return { post, posts, comments, getPost, getPosts, getCommentsForPost }
+  interface PostRate {
+    [id: number]: ReactionType
+  }
+
+  const getPostsRatesFromStorage = (): PostRate => {
+    const postsRates = useCookie<PostRate>('APP_postsRates')
+    return postsRates.value
+  }
+
+  const setPostsRatesToStorage = (postsRates: PostRate) => {
+    const cookie = useCookie<PostRate>('APP_postsRates')
+    cookie.value = postsRates
+  }
+
+  const setRatedPosts = () => {
+    const postsRates = getPostsRatesFromStorage()
+    if (postsRates) {
+      for (const [id, type] of Object.entries(postsRates)) {
+        const postFromList = posts.value.find(item => item.id === Number(id))
+        const currentPost = post.value.id === Number(id) ? post.value : null
+        if (postFromList) {
+          postFromList.userRate = type
+        }
+        if (currentPost) {
+          currentPost.userRate = type
+        }
+      }
+    }
+  }
+
+  const ratePost = (type: ReactionType, id: number) => {
+    const postFromList = posts.value.find(item => item.id === id)
+    const currentPost = post.value.id === id ? post.value : null
+    let isRateRemoved = false
+    if (postFromList) {
+      if (postFromList.userRate === type) {
+        delete postFromList.userRate
+        isRateRemoved = true
+      }
+      else {
+        postFromList.userRate = type
+      }
+    }
+    if (currentPost) {
+      if (currentPost.userRate === type) {
+        delete currentPost.userRate
+        isRateRemoved = true
+      }
+      else {
+        currentPost.userRate = type
+      }
+    }
+    const postsRates = getPostsRatesFromStorage()
+    if (postsRates) {
+      if (isRateRemoved) {
+        delete postsRates[id]
+      }
+      else {
+        postsRates[id] = type
+      }
+      postsRates[id] = type
+      setPostsRatesToStorage(postsRates)
+    }
+    else {
+      if (isRateRemoved)
+        return false
+      setPostsRatesToStorage({ [id]: type })
+    }
+  }
+
+  return {
+    post,
+    posts,
+    getPost,
+    getPosts,
+    ratePost,
+    setRatedPosts,
+    comments,
+    getCommentsForPost,
+    setRemovedComments,
+    removeComment,
+    restoreComment
+  }
 })
