b6c206286745e76acbaf9f3f17dfca68400f74a0
toda1337 <toda.corsa@gmail.com>
2024-11-02 02:09:27 +0300

    Changed index page, added api calls for get posts and show posts in each Post components

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    {{ posts }}
+    <Post
+      v-for="item in posts"
+      :key="item.id" :post="item"
+      :is-title-link="true"
+      :has-comments-link="true"
+      class="mb-6.5 last:mb-0"
+      @rate="ratePost($event, item.id)"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import Post from '@/components/shared/fragments/post.vue'
+
 const postStore = usePostStore()
 const posts = await postStore.getPosts()
+const { ratePost, setRatedPosts } = postStore
+
+useSeo()
+setRatedPosts()
 </script>
